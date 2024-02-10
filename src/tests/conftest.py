3683da67290bee6d80e65079b0ecff34a96dd090
Eloi Dieme <eloidieme@gmail.com>
2024-02-10 16:10:15 +0100

    fix: refactor logging to use the utils.logger file and ensure separation of concerns
    Refs: #10

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,14 +1,6 @@
 import pytest 
 from main import create_app
-from logging import Handler, LogRecord
-
-class LogCaptureHandler(Handler):
-    def __init__(self):
-        super().__init__()
-        self.records = []
-
-    def emit(self, record: LogRecord):
-        self.records.append(record)
+from utils.logger import LogCaptureHandler
 
 @pytest.fixture
 def app():
