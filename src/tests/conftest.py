7d69abf74f16ba8ca92ffd57bde540c1bf91328c
Eloi Dieme <115150162+eloidieme@users.noreply.github.com>
2024-02-12 14:01:54 +0100

    Configure logging (#24)
    * feat: configure logging and test it
    
    Added a console handler and a file handler for logging.
    Now logging is done in the console and saved to files.
    Added logs/ directory to .gitignore.
    Tested logging with an info message that logs when accessing the home page in debug mode.
    
    Refs: #10
    
    * fix: add testing with fixtures to test logging feature with pytest directly
    
    Refs: #10
    
    * fix: refactor logging to use the utils.logger file and ensure separation of concerns
    
    Refs: #10

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,10 +1,12 @@
 import pytest 
 from main import create_app
+from utils.logger import LogCaptureHandler
 
 @pytest.fixture
 def app():
     app = create_app({
-        'TESTING': True
+        'TESTING': True,
+        'DEBUG': True
     })
 
     yield app
@@ -16,3 +18,11 @@ def client(app):
 @pytest.fixture()
 def runner(app):
     return app.test_cli_runner()
+
+@pytest.fixture
+def captured_logs(app):
+    logger = app.logger
+    handler = LogCaptureHandler()
+    logger.addHandler(handler)
+    yield handler.records
+    logger.removeHandler(handler)
--- src/tests/test_ci_server.py
+++ src/tests/test_ci_server.py
@@ -1,3 +1,12 @@
 def test_webhook(client):
     response = client.get('/server/')
     assert b"<title>CI Server</title>" in response.data
+
+def test_logging(app, captured_logs):
+    app.logger.info("This is an info log")
+    app.logger.warning("This is a warning log")
+    app.logger.error("This is an error log")
+
+    assert any("This is an info log" in record.msg for record in captured_logs)
+    assert any("This is a warning log" in record.msg for record in captured_logs)
+    assert any("This is an error log" in record.msg for record in captured_logs)
