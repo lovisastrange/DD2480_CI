9dd1123c1bcb6f9639b1981e2329f0e3cbc3466e
lovisastrange <82108064+lovisastrange@users.noreply.github.com>
2024-02-12 17:38:35 +0100

    14  - pylint syntax check (#26)
    * feat: Added syntax checker class
    
    Added syntax checker class to check syntax of a program, as well as
    added tests and data to use in the tests
    
    * Update python-app.yml to exclude test files
    
    * fix: fix tests and specifies pylint version in requirements, otherwise won't run #26
    
    ---------
    
    Co-authored-by: Eloi Dieme <115150162+eloidieme@users.noreply.github.com>
    Co-authored-by: Eloi Dieme <eloidieme@gmail.com>

---
+++ src/main/syntax_checker.py
@@ -0,0 +1,66 @@
+from pylint.lint import Run
+from io import StringIO  
+import sys
+
+class Syntax_Checker:
+    """
+    Class for doing a static syntax check on all code in the project
+    
+    
+    methods:
+    do_syntax_check(path): runs pylint on the code in the given path, only checking for errors
+    do_additional_check(path): runs pylint on the code in the given path
+    """
+
+    def __init__(self):
+        self.message = ""
+
+
+
+    def do_syntax_check(self,path):
+        ## Input: Path of files to be checked
+        ## Output: Sets message about syntax check status
+
+        output = StringIO()  # Captures output from stdout
+        old_stdout = sys.stdout
+        sys.stdout = output
+       
+        try:
+            report_format = "--msg-template='{C}:{line:3d},{column}: {obj}: {msg}'" # Sets format for pylint report
+            only_errors = "--disable=R,C,W" # Only checks for errors
+
+            result = Run([path,only_errors,report_format],do_exit=False)
+
+            errors = result.linter.stats.by_msg.get('syntax-error', 0)
+
+            if errors == 0:
+                self.message += "The code passed the check with no syntax errors \n\n"
+                self.do_additional_check(path)  # Does additional checks for warnings
+            else:
+                self.message += "The code contains syntax errors. \n\n"
+
+            # Formats output
+            text = output.getvalue()
+
+        finally:
+            sys.stdout = old_stdout
+            output.close()
+
+        parts = text.split("------------------------------------------------------------------")
+        
+        if len(parts) <=2:
+            self.message += parts[0]
+        
+        else:
+            warnings = parts[-2].split("\n\n*")
+            if len(warnings) >= 2:
+                self.message += warnings[1]
+
+    def do_additional_check(self,path):
+        ## Input: Path of files to be checked
+        ## Output: Does another round of checks on the code
+
+        report_format = "--msg-template='{C}:{line:3d},{column}: {obj}: {msg}'"
+        Run([path,report_format],do_exit=False)
+
+        self.message += "The following warnings and comments were found for the code \n"
