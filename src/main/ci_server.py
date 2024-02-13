5be8e3aadb526fc2e4af02d608abc15c54ab2a24
flug0 <hugo@u1.se>
2024-02-13 20:04:01 +0100

    feat Discord bot for CI notifications #31

--- src/main/ci_server.py
+++ src/main/ci_server.py
@@ -3,9 +3,14 @@ from .webhook_handler import Webhook_handler
 from .db import query_builds, query_build
 from .builder import Builder
 from dotenv import load_dotenv
+from ..discord_bot.main import CI_notificator
 import os
+import threading
 
 bp = Blueprint('ci_server', __name__, url_prefix='/server')
+notificator = CI_notificator()
+notificator_thread = threading.Thread(target=notificator.bot.run, args=(notificator.token,))
+notificator_thread.start()
 
 @bp.route('/', methods=["GET"])
 def home():
@@ -44,6 +49,7 @@ def ci_process(data, token):
     builder = Builder(data)
     build = builder.build()
     builder.send_status(data, build, token)
+    notificator.send_notification(f"Build {build['id']} finished with status {build['status']}")
 
 @bp.errorhandler(500)
 def handle_500(error):
