998332624d72f8889b82ddaba9fb571a6c9bca07
flug0 <hugo@u1.se>
2024-02-13 13:03:24 +0100

    Add Discord bot notification feature

--- src/discord_bot/main.py
+++ src/discord_bot/main.py
@@ -7,10 +7,14 @@ class CI_notificator:
     def __init__(self):
         self.token = self.get_token()
         self.bot = self.setup()
+        self.channelID = 1205521583095947264 # ID for ci channel in the discord server
 
     def setup(self):
         intents = discord.Intents.default()
         intents.message_content = True
+        intents.members = True
+        intents.presences = True
+        intents.guilds = True
         bot = commands.Bot(command_prefix="!", intents=intents)
 
         @bot.command()
@@ -19,6 +23,14 @@ class CI_notificator:
             await ctx.send('pong')
         
         return bot
+    
+    async def send_notification(self, message):
+        channel = self.bot.get_channel(self.channelID)
+        if channel:  # Check if the channel was found
+            print("Sending message")
+            await channel.send(message)
+        else:
+            print("Channel not found")
 
     def get_token(self):
         load_dotenv("tokens.env")
