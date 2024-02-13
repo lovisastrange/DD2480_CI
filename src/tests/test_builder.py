36053ad40016f37bf02ee3aae73384b37c96b311
OllanBollan <84289304+OllanBollan@users.noreply.github.com>
2024-02-13 14:30:24 +0100

    20 builder class cloning the code (#32)
    * feat: added functionality to clone repo
    
    Added functionality to clone repo from github and test class to test the build code
    
    * feat: Added subprocesses
    
    Added subprocesses to install requirements and run pytests in cloned repo directory
    
    * Added code comments
    
    Added code comments to test and build functions
    
    * Update webhook_handler.py
    
    Added clone URL to webhook_handler.py output
    
    * Fix: clone_url in test_builder.py
    
    Fixed to correct key name clone_url in mock data dict
    
    * fix some tests in webhook_handler and builder #20
    
    ---------
    
    Co-authored-by: Eloi Dieme <eloidieme@gmail.com>

---
+++ src/tests/test_builder.py
@@ -0,0 +1,28 @@
+from unittest.mock import patch
+import pytest
+from src.main.builder import Builder
+import os
+
+def test_clone_repo():
+    #Input: A mock dictionary containing repository information, including repository URL, branch, and commit.
+    #Verifies that the clone_repo method correctly calls subprocess.run with the appropriate arguments
+    #when cloning a repository from GitHub.
+    data = {
+        "repo": "https://github.com/user/repo.git",
+        "clone_url": "https://github.com/user/repo.git",
+        "commit": "123abc",
+        "branch": "main"
+    }
+    builder = Builder(data)
+    repo_path = os.path.join(os.getcwd(), builder.repo)
+
+    with patch('subprocess.run') as mock_run:
+        # Call the clone_repo method
+        builder.clone_repo(data["repo"],data["branch"],data["clone_url"])
+        
+        # Assert that subprocess.run was called with the correct arguments
+        mock_run.assert_called_with(
+            ["git", "clone", "--single-branch", "--branch", "main", data["clone_url"], f"{repo_path}"],check=True)
+
+if __name__ == "__main__":
+    pytest.main([__file__])
--- src/tests/test_webhook_handler.py
+++ src/tests/test_webhook_handler.py
@@ -7,7 +7,7 @@ os.environ['WEBHOOK_SECRET'] = 'valid_signature'
 def test_verify_success(client, mocker):
     mocker.patch('main.webhook_handler.hmac.new', return_value=mocker.Mock(hexdigest=lambda: 'valid_signature'))
     data = {
-        'repository': {'name': 'test_repo'},
+        'repository': {'name': 'test_repo', 'clone_url':'unknow'},
         'after': 'test_commit',
         'ref': 'refs/heads/test_branch'
     }
@@ -24,13 +24,14 @@ def test_verify_failure(client, mocker):
 def test_parse_data_success(client, mocker):
     mocker.patch('main.webhook_handler.hmac.new', return_value=mocker.Mock(hexdigest=lambda: 'valid_signature'))
     data = {
-        'repository': {'name': 'test_repo'},
+        'repository': {'name': 'test_repo', 'clone_url':'unknown'},
         'after': 'test_commit',
         'ref': 'refs/heads/test_branch'
     }
     response = client.post('/server/webhook', headers={'X-Hub-Signature-256': f'sha256=valid_signature', 'Content-Type': 'application/json'}, json=data)
     expected = {
         "repo": "test_repo",
+        "clone_url": "unknown",
         "commit": "test_commit",
         "branch": "test_branch"
     }
