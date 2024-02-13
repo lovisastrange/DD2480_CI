1634bbe05f65027ad0e7caa73d3309ac2cb7a9df
Flug0 <92332371+Flug0@users.noreply.github.com>
2024-02-13 13:51:57 +0100

    Discord bot (#30)
    * Initialized bot
    
    * Initialized bot
    
    * initialized bot
    
    * Delete src/discord_bot/tokens.env
    
    * Add Discord bot notification feature
    
    * Add test for send_notification method in Discord bot
    
    * Add test command used via discord to test send_notification
    
    * Add pytest-asyncio to requirements.txt
    
    * fix: add discord package to requirements #4
    
    ---------
    
    Co-authored-by: Eloi Dieme <eloidieme@gmail.com>

---
+++ src/discord_bot/main.py
@@ -0,0 +1,48 @@
+import os
+from discord.ext import commands
+from dotenv import load_dotenv
+import discord
+
+class CI_notificator:
+    def __init__(self):
+        self.token = self.get_token()
+        self.bot = self.setup()
+        self.channelID = 1205521583095947264 # ID for ci channel in the discord server
+
+    def setup(self):
+        intents = discord.Intents.default()
+        intents.message_content = True
+        intents.members = True
+        intents.presences = True
+        intents.guilds = True
+        bot = commands.Bot(command_prefix="!", intents=intents)
+
+        @bot.command()
+        async def ping(ctx):
+            print(ctx)
+            await ctx.send('pong')
+        
+        
+    
+        @bot.command()
+        async def test(ctx):
+            print(ctx)
+            await self.send_notification("Test message")
+
+        return bot
+    
+    async def send_notification(self, message):
+        channel = self.bot.get_channel(self.channelID)
+        if channel:  # Check if the channel was found
+            print("Sending message")
+            await channel.send(message)
+        else:
+            print("Channel not found")
+
+    def get_token(self):
+        load_dotenv("tokens.env")
+        return os.getenv('DISCORD_BOT_TOKEN')
+
+if __name__ == "__main__":
+    notificator = CI_notificator()
+    notificator.bot.run(notificator.token)
