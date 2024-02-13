22b2f30c6038ccb6226cd9983fcf670b3595b19d
Eloi Dieme <eloidieme@gmail.com>
2024-02-14 00:20:56 +0100

    fix: modified notification test to mock the response so Github Actions can work without the discord webhook link #4

--- src/tests/test_notification.py
+++ src/tests/test_notification.py
@@ -1,6 +1,20 @@
+import unittest
+from unittest.mock import patch
 from main.notification import send_notification_webhook
 
-def test_notification():
-    message = "Test message"
-    response = send_notification_webhook(message)
-    assert response == 204
+class TestNotification(unittest.TestCase):
+
+    @patch('main.notification.requests.post')
+    def test_notification(self, mock_post):
+        mock_response = mock_post.return_value
+        mock_response.status_code = 204
+
+        message = "Test message"
+        response = send_notification_webhook(message)
+
+        mock_post.assert_called_once()
+        
+        self.assertEqual(response, 204)
+
+if __name__ == '__main__':
+    unittest.main()
